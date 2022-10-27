4e8f347a31c44336d30c937412deaba62d891689
Luna123j <58311487+Luna123j@users.noreply.github.com>
2022-10-26 21:53:43 -0400

    add update functions

--- tut-project/mysite/members/views.py
+++ tut-project/mysite/members/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.template import loader
 from .models import Members
+from django.urls import reverse
+
 
 def index(request):
     # return HttpResponse(template.render())
@@ -11,3 +13,36 @@ def index(request):
       'mymembers': mymembers,
     }
     return HttpResponse(template.render(context, request))
+
+def add(request):
+  template = loader.get_template('add.html')
+  return HttpResponse(template.render({}, request))
+
+def addrecord(request):
+  x = request.POST['first']
+  y = request.POST['last']
+  member = Members(firstname=x, lastname=y)
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
+
+def delete(request, id):
+  member = Members.objects.get(id=id)
+  member.delete()
+  return HttpResponseRedirect(reverse('index'))
+
+def update(request, id):
+  mymember = Members.objects.get(id=id)
+  template = loader.get_template('update.html')
+  context = {
+    'mymember': mymember,
+  }
+  return HttpResponse(template.render(context, request))
+
+def updaterecord(request, id):
+  first = request.POST['first']
+  last = request.POST['last']
+  member = Members.objects.get(id=id)
+  member.firstname = first
+  member.lastname = last
+  member.save()
+  return HttpResponseRedirect(reverse('index'))
